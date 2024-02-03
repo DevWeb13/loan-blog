8061e4c9e92c843077d39b92d00659039c9dead2
DevWeb13 <giuliano.loic@sfr.fr>
2024-02-03 10:49:30 +0100

    Delete config

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,15 +1,23 @@
-import { FooterCopyright, Section } from 'astro-boilerplate-components';
-
-import { AppConfig } from '@/utils/AppConfig';
+import { Section } from 'astro-boilerplate-components';
 
 const Footer = () => (
   <Section>
-    <FooterCopyright site_name={AppConfig.author} />
     <p>
-      &copy; Copyright {new Date().getFullYear()} par{' '}
-      <a href="https://lareponsedev.com" target="_blank">
+      &copy; Copyright {new Date().getFullYear()}{' '}
+      <a
+        href='https://lareponsedev.com'
+        target='_blank'
+        className='text-cyan-400 hover:underline'>
         LaReponseDev
       </a>
+      . Design{' '}
+      <a
+        href='https://creativedesignsguru.com/'
+        target='_blank'
+        className='text-cyan-400 hover:underline'>
+        CreativeDesignsGuru
+      </a>
+      .
     </p>
   </Section>
 );
